648ebd63a1b5e479250f67c79668c06a8b89df48
ltvhcung001 <ltv.h.cung001@gmail.com>
2025-12-16 21:42:21 +0700

    Update HW1.sql

--- HW1.sql
+++ HW1.sql
@@ -4,11 +4,11 @@ CREATE TABLE Orders (
     order_date DATE,
     total_amount DECIMAL(10, 2)
 );
--- 2a. -- 2. Thực hiện truy vấn SELECT * FROM Orders WHERE customer_id = X; trước khi tạo Index
+-- 2a. Thực hiện truy vấn SELECT * FROM Orders WHERE customer_id = X; trước khi tạo Index
 SELECT * FROM Orders WHERE customer_id = 100;
 
 -- 1. Tạo một B-Tree Index trên cột customer_id
 CREATE INDEX idx_customer_id ON Orders(customer_id);
 
--- 2. Thực hiện truy vấn SELECT * FROM Orders WHERE customer_id = X; sau khi tạo Index, so sánh thời gian thực hiện
+-- 2b. Thực hiện truy vấn SELECT * FROM Orders WHERE customer_id = X; sau khi tạo Index, so sánh thời gian thực hiện
 SELECT * FROM Orders WHERE customer_id = 100;
